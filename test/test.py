d9821e8eb758c63b319c35dbe805b0223835ea9a
Quinten <quinten.roets@gmail.com>
2022-01-04 11:39:06 +0100

    add yaml test

---
+++ test/test.py
@@ -0,0 +1,11 @@
+from plib import Path
+
+
+def test_yaml():
+    p = Path("testpath")
+    d = {"hi": "there"}
+    p.save(d)
+    try:
+        assert d == p.load()
+    finally:
+        p.unlink()

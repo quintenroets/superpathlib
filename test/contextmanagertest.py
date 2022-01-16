9c60ce6d5fa914af3411943d8c62f34e225fa4cc
Quinten <quinten.roets@gmail.com>
2022-01-16 16:38:56 +0100

    use single quotes

--- test/contextmanagertest.py
+++ test/contextmanagertest.py
@@ -1,8 +1,8 @@
 from plib import Path
 import time
 
-path = Path.docs / "test.txt"
+path = Path.docs / 'test.txt'
 
-with path.with_suffix(".yaml") as temp_path:
+with path.with_suffix('.yaml') as temp_path:
     print(temp_path)
     time.sleep(2)
--- test/yaml_test.py
+++ test/yaml_test.py
@@ -2,8 +2,8 @@ from plib import Path
 
 
 def test_yaml():
-    p = Path("testpath")
-    d = {"hi": "there"}
+    p = Path('testpath')
+    d = {'hi': 'there'}
     p.save(d)
     try:
         assert d == p.load()

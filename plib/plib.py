7528bd57574a5923ecba89979e2e90cf9a2951fd
Quinten <quinten.roets@gmail.com>
2022-01-24 15:01:01 +0100

    add autoformatting

--- plib/plib.py
+++ plib/plib.py
@@ -21,7 +21,9 @@ def catch_missing(default=None):
             except FileNotFoundError:
                 res = default
             return res
+
         return wrap_args
+
     return wrap_function
 
 
@@ -35,6 +37,7 @@ def create_parents(func):
             path.parent.mkdir(parents=True, exist_ok=True)
             res = func(*args, **kwargs)
         return res
+
     return wrapper
 
 
@@ -42,12 +45,15 @@ class Path(BasePath):
     """
     Extend pathlib functionality and enable further extensions by inheriting
     """
-    _flavour = _windows_flavour if os.name == 'nt' else _posix_flavour  # _flavour attribute needs to inherited explicitely from pathlib
 
+    _flavour = (
+        _windows_flavour if os.name == "nt" else _posix_flavour
+    )  # _flavour attribute needs to inherited explicitely from pathlib
 
     """
     Overwrite existing methods with exception handling and default values
     """
+
     @create_parents
     def touch(self, mode=0o666, exist_ok=True, mtime=None):
         super().touch(mode=mode, exist_ok=exist_ok)
@@ -61,123 +67,130 @@ class Path(BasePath):
     def iterdir(self, missing_ok=True):
         if self.exists() or not missing_ok:
             yield from super().iterdir()
-    
+
     def rename(self, target):
         target.parent.mkdir(parents=True, exist_ok=True)
         return super().rename(target)
 
-    def open(self, mode='r', **kwargs):
+    def open(self, mode="r", **kwargs):
         try:
             res = super().open(mode, **kwargs)
         except FileNotFoundError:
-            if 'w' in mode or 'a' in mode:
+            if "w" in mode or "a" in mode:
                 # exist_ok=True: catch race conditions when calling multiple times
                 self.parent.mkdir(parents=True, exist_ok=True)
                 res = super().open(mode, **kwargs)
-            elif 'b' in mode:
-                res = io.BytesIO(b'')
+            elif "b" in mode:
+                res = io.BytesIO(b"")
             else:
-                res = io.StringIO('')
+                res = io.StringIO("")
             return res
         return res
-    
-    
+
     """
     Properties to read & write content in different formats
     """
+
     @property
     def byte_content(self) -> bytes:
         return self.read_bytes()
-    
+
     @byte_content.setter
     def byte_content(self, value: bytes) -> None:
         return self.write_bytes(value)
-    
+
     @property
     def text(self) -> str:
         return self.read_text()
-    
+
     @text.setter
     def text(self, value: str) -> None:
         return self.write_text(value)
-    
+
     @property
     def lines(self) -> List[str]:
-        lines = self.text.strip().split('\n')
+        lines = self.text.strip().split("\n")
         lines = [l for l in lines if l]
         return lines
-    
+
     @lines.setter
     def lines(self, lines: List[str]) -> None:
-        self.text = '\n'.join(lines)
-    
+        self.text = "\n".join(lines)
+
     @property
     def json(self):
         import json
-        return json.loads(self.text or '{}')
-        
+
+        return json.loads(self.text or "{}")
+
     @json.setter
     def json(self, content):
         import json
+
         self.text = json.dumps(content)
-        
+
     @property
     def yaml(self):
         import yaml
+
         # C implementation much faster but only supported on Linux
-        Loader = yaml.CFullLoader if hasattr(yaml, 'CFullLoader') else yaml.FullLoader
+        Loader = yaml.CFullLoader if hasattr(yaml, "CFullLoader") else yaml.FullLoader
         return yaml.load(self.text, Loader=Loader) or {}
 
     @yaml.setter
     def yaml(self, value):
         import yaml
+
         # C implementation much faster but only supported on Linux
-        Dumper = yaml.CDumper if hasattr(yaml, 'CDumper') else yaml.Dumper  
+        Dumper = yaml.CDumper if hasattr(yaml, "CDumper") else yaml.Dumper
         self.text = yaml.dump(value, Dumper=Dumper)
-    
+
     @property
     def content(self):
-        return self.with_suffix('.yaml').yaml
-    
+        return self.with_suffix(".yaml").yaml
+
     @content.setter
     def content(self, value):
-        self.with_suffix('.yaml').yaml = value
+        self.with_suffix(".yaml").yaml = value
 
-    
     """
     Properties to read & write metadata
     """
+
     @property
     @catch_missing(default=0.0)
     def mtime(self):
         return self.stat().st_mtime
-        
+
     @mtime.setter
     def mtime(self, time: float):
-        os.utime(self, (time, time)) # set create time as well
+        os.utime(self, (time, time))  # set create time as well
         import subprocess
+
         try:
-            subprocess.run(('touch', '-d', f'@{time}', self))
+            subprocess.run(("touch", "-d", f"@{time}", self))
         except subprocess.CalledProcessError:
-            pass # Doesn't work on Windows
-        
+            pass  # Doesn't work on Windows
+
     @property
     def tags(self):
         from .tags import XDGTags
+
         return XDGTags(self).get()
-    
+
     @tags.setter
     def tags(self, *values):
         from .tags import XDGTags
+
         if len(values) == 1 and values[0] == None:
             XDGTags(self).clear()
         else:
             XDGTags(self).set(*values)
-            
+
     @property
     def tag(self):
         return self.tags[0] if self.tags else None
-    
+
     @tag.setter
     def tag(self, value):
         self.tags = value
@@ -192,12 +205,12 @@ class Path(BasePath):
         path = self
         while not path.exists():
             path = path.parent
-        return path.owner() == 'root'
-    
-    
+        return path.owner() == "root"
+
     """
     Additional functionality
     """
+
     def copy_to(self, dest):
         dest.byte_content = self.byte_content
 
@@ -215,22 +228,35 @@ class Path(BasePath):
             self.write_bytes(content)
         else:
             self.save(content)
-    
+
     def load(self, trusted=False):
         """
         :param trusted: if the path is trusted, an unsafe loader can be used to instantiate any object
         :return: Content in path that contains yaml format
         """
-        import yaml # lazy import
-        
-        path = self.with_suffix('.yaml')
+        import yaml  # lazy import
+
+        path = self.with_suffix(".yaml")
         loader = yaml.CUnsafeLoader if trusted else yaml.CFullLoader
         return yaml.load(path.text, Loader=loader) or {}
-    
+
     def save(self, content):
         self.content = content
 
-    def find(self, condition=None, exclude=None, recurse_on_match=False, follow_symlinks=True, only_folders=False):
+    def update(self, value):
+        self.content = self.content | value
+
+    def __ior__(self, other):
+        return self.update(other)
+
+    def find(
+        self,
+        condition=None,
+        exclude=None,
+        recurse_on_match=False,
+        follow_symlinks=False,
+        only_folders=False,
+    ):
         """
         Find all subpaths under path that match condition
 
@@ -243,6 +269,7 @@ class Path(BasePath):
                 return True
 
         if exclude is None:
+
             def exclude(_):
                 return False
 
@@ -274,7 +301,6 @@ class Path(BasePath):
         if remove_root:
             self.rmdir()
 
-    
     @classmethod
     def tempfile(cls, **kwargs) -> Path:
         """
@@ -285,48 +311,50 @@ class Path(BasePath):
             process_logs(logs)
         """
         import tempfile
+
         _, path = tempfile.mkstemp(**kwargs)
         return cls(path)
-    
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self, *_):
         self.unlink()
-    
+
     """
     Common folders: properties with classmethods such that 
     all child classes have common folders with all the right properties and methods
     """
+
     @classmethod
     @property
     def HOME(cls) -> Path:
         return cls.home()
-    
+
     @classmethod
     @property
     def docs(cls) -> Path:
-        return cls.HOME / 'Documents'
-    
+        return cls.HOME / "Documents"
+
     @classmethod
     @property
     def scripts(cls) -> Path:
-        return cls.docs / 'Scripts'
-    
+        return cls.docs / "Scripts"
+
     @classmethod
     @property
     def script_assets(cls) -> Path:
-        return cls.HOME / '.config' / 'scripts'
-    
+        return cls.HOME / ".config" / "scripts"
+
     @classmethod
     @property
     def assets(cls) -> Path:
         """
         Often overwritten by child classes for specific project
         """
-        return cls.HOME / '.config' / 'scripts'
-    
+        return cls.HOME / ".config" / "scripts"
+
     @classmethod
     @property
     def draft(cls) -> Path:
-        return cls.docs / 'draft.txt'
+        return cls.docs / "draft.txt"

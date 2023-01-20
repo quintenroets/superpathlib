7e8dab74d8cd9513a286bd84930e35fb6b33b275
Quinten <quinten.roets@gmail.com>
2023-01-19 20:14:43 -0500

    new release

--- setup.py
+++ setup.py
@@ -18,12 +18,14 @@ setup(
     author="Quinten Roets",
     author_email="quinten.roets@gmail.com",
     url="https://github.com/quintenroets/superpathlib",
-    download_url="https://github.com/quintenroets/superpathlib/archive/refs/tags/v0.0.2.tar.gz",
+    download_url=(
+        "https://github.com/quintenroets/superpathlib/archive/refs/tags/v0.0.2.tar.gz"
+    ),
     description="extended pathlib",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     name=NAME,
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=read("requirements.txt"),
 )

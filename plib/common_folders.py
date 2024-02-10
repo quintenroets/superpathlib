fce31aa6d73b60f68bc81ee6207f24152cfad667
Quinten <quinten.roets@gmail.com>
2024-02-10 11:01:54 -0800

    fix typing information for common folders

--- plib/common_folders.py
+++ plib/common_folders.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from typing import TypeVar
+
 from simple_classproperty import classproperty
 
 from . import base
+
+T = TypeVar("T", bound="Path")
 
 
 class Path(base.Path):
@@ -14,29 +18,35 @@ class Path(base.Path):
     since python3.11.
     """
 
+    @classmethod
     @classproperty
-    def HOME(cls) -> Path:
+    def HOME(cls: type[T]) -> T:
         return cls.home()
 
+    @classmethod
     @classproperty
-    def docs(cls) -> Path:
+    def docs(cls: type[T]) -> T:
         return cls.HOME / "Documents"
 
+    @classmethod
     @classproperty
-    def scripts(cls) -> Path:
+    def scripts(cls: type[T]) -> T:
         return cls.docs / "Scripts"
 
+    @classmethod
     @classproperty
-    def script_assets(cls) -> Path:  # noqa
+    def script_assets(cls: type[T]) -> T:
         return cls.scripts / "assets"
 
+    @classmethod
     @classproperty
-    def assets(cls) -> Path:  # noqa
+    def assets(cls: type[T]) -> T:
         """
         Often overwritten by child classes for specific project.
         """
-        return cls.script_assets  # noqa
+        return cls.script_assets
 
+    @classmethod
     @classproperty
-    def draft(cls) -> Path:  # noqa
+    def draft(cls: type[T]) -> T:
         return cls.docs / "draft.txt"

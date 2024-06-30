52df4d92c7271c9e05c9fa452e04e4e8e798006c
Quinten <quinten.roets@gmail.com>
2024-06-30 02:18:56 +0000

    use ruff best practices (#27)

---
+++ src/superpathlib/cli/entry_point.py
@@ -0,0 +1,6 @@
+from package_utils.context.entry_point import create_entry_point
+
+from superpathlib import main
+from superpathlib.context import context
+
+entry_point = create_entry_point(main, context)

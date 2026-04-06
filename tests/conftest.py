3e148af5238e558da2660b7897b996a7a332246e
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2026-04-05 19:01:03 -0700

    introduce-configurable-askpass (#52)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,16 @@
 from collections.abc import Iterator
+from unittest.mock import patch
 
 import pytest
 
 from superpathlib import Path
 from superpathlib.encryption import EncryptedPath
+
+
+@pytest.fixture(autouse=True, scope="session")
+def encryption_password() -> Iterator[None]:
+    with patch.dict("os.environ", {"FILE_ENCRYPTION_PASSWORD": "test_password"}):
+        yield
 
 
 def provision_path(*, in_memory: bool = False) -> Iterator[Path]:
